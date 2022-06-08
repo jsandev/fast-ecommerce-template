13c454af076f13a728dc351249884f5eb9cfe6f1
Jimmy Sandoval <jsandovaldev.pe@gmail.com>
2022-06-07 22:15:23 -0500

    Init and config project

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex w-full h-full justify-center items-center">Fast Ecommerce Template</div>
+  );
+};
+
+export default Home;
