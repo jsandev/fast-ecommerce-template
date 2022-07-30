6a432e2c766b287420d3d61caabf630326a3e916
Jimmy Sandoval <jsandovaldev.pe@gmail.com>
2022-07-29 19:45:11 -0500

    Add styles to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,43 @@
+import Link from "next/link";
+import { FiHeart, FiUser, FiShoppingCart } from "react-icons/fi";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="w-full h-[80px] flex items-center justify-center">
+      <div className="w-full max-w-[1200px] mx-auto flex items-center justify-between">
+        <Link href="/">
+          <a className="font-bold text-[1.5rem] text-turquoise">JSandoval.</a>
+        </Link>
+        <ul className="list-none flex items-center gap-6">
+          <li>
+            <Link href="/">
+              <a className="relative">
+                <FiHeart size={24} />
+                <span className="w-[1rem] h-[1rem] absolute -top-[6px] -right-[6px] flex justify-center items-center bg-red rounded-circle font-semibold text-[0.625rem] text-white">
+                  3
+                </span>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a className="relative">
+                <FiShoppingCart size={24} />
+                <span className="w-[1rem] h-[1rem] absolute -top-[6px] -right-[6px] flex justify-center items-center bg-red rounded-circle font-semibold text-[0.625rem] text-white">
+                  5
+                </span>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>
+                <FiUser size={24} />
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 };
